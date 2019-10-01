ec036ded7f45c4c6da88cce6c487292a36adb40b
Akanksha Singh <akankshasingh121297@gmail.com>
2019-10-01 13:25:40 +0530

    Initial

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=hackdaybot1-bot-Web-Deploy.pubxml -p:Password=Mmkuk4uiTKNajbGiTHRzclpdxQumd8aqt9BCSgerRYrDlGDKNlwvYBfM3tH0
+
